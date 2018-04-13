de61e9a5afca0c5e9d7f133b333e4b19491a0d48
Severo <severo.fabricio@gmail.com>
2018-04-13 00:01:24 -0300

    Adds page header component

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import PageHeader from '../template/page.header'
+
 export default props => (
   <div>
-    <h1>About!</h1>
+    <PageHeader name="About" small="us" />
+    <h2>Our history</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus facilis saepe perferendis quod quibusdam, accusamus quisquam. Natus, voluptatibus? Dolores nostrum repellat, similique impedit cupiditate officia placeat error deserunt accusantium id!</p>
+    <h2>Mission and Vision</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab magnam dignissimos voluptas dicta atque similique! Explicabo fuga, inventore nam sit dicta quam dolor porro recusandae esse sint! Magni, quod ea.</p>
+    <h2>Press</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt saepe atque explicabo asperiores accusamus eum, harum delectus similique, fuga sequi quia error eligendi! Autem eveniet magnam, ea id harum delectus.</p>
   </div>
 )
