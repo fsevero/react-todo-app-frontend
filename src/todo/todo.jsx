ba9bb00abf2243bd2d77d9f8970bf921b61c89a1
Severo <severo.fabricio@gmail.com>
2018-04-13 00:05:38 -0300

    Adds Form and List dumb components

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -2,11 +2,16 @@ import React, { Component } from 'react'
 
 import PageHeader from '../template/page.header'
 
+import TodoForm from './todo.form'
+import TodoList from './todo.list'
+
 export default class Todo extends Component {
   render() {
     return (
       <div>
         <PageHeader name="Tasks" small="form" />
+        <TodoForm />
+        <TodoList />
       </div>
     )
   }
