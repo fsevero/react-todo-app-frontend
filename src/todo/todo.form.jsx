fbe37d478ca0bd6dfbd3b2faccf974b0f4875464
Severo <severo.fabricio@gmail.com>
2018-04-13 21:12:21 -0300

    Handling events between components

--- src/todo/todo.form.jsx
+++ src/todo/todo.form.jsx
@@ -7,11 +7,16 @@ export default props => (
   <div role="form" className="todo-form">
 
     <Grid cols="12 9 10">
-      <input type="text" id="description" className="form-control" placeholder="Adicione uma tarefa"/>
+      <input type="text"
+             id="description"
+             className="form-control"
+             placeholder="Adicione uma tarefa"
+             value={props.description}
+             onChange={props.handleChange}/>
     </Grid>
 
     <Grid cols="12 3 2">
-      <IconButton style="primary" icon="plus" />
+      <IconButton style="primary" icon="plus" onClick={props.handleAdd} />
     </Grid>
 
   </div>
